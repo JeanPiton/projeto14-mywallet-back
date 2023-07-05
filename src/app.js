b90d999b6d748944e8ee0bcad2cd8f0b7d02e1d7
JeanPiton <jean.orpiton@gmail.com>
2023-07-05 18:10:44 -0300

    Implement route post users

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import express, { json } from "express"
 import cors from "cors"
 import dotenv from "dotenv"
-import { MongoClient, MongoClient, MongoClient } from "mongodb"
+import { MongoClient } from "mongodb"
 import joi from "joi"
+import bcrypt from "bcrypt"
 dotenv.config()
 
 const PORT = 5000
@@ -12,6 +13,27 @@ server.use(json())
 server.listen(PORT,()=>{console.log(`Server running at http://localhost:${PORT}`)})
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL)
-.then(mongoClient.connect())
+let db
+mongoClient.connect()
+.then(()=>db = mongoClient.db())
 .catch(err=>{console.log(err)})
-const db = mongoClient.db()
+
+
+server.post("/users",async (req,res)=>{
+    const {name, email, password} = req.body
+    const scheme = joi.object({
+        name:joi.string().required(),
+        email:joi.string().email().required(),
+        password:joi.string().required().min(3)
+    })
+    const validation = scheme.validate({name,email,password})
+    try{
+        if(validation.error) return res.status(422).send(validation.error.message)
+        if(await db.collection("users").findOne({email:email})) return res.status(409).send("user already exists")
+        const hash = bcrypt.hashSync(password,10)
+        db.collection("users").insertOne({name:name,email:email,password:hash})
+        return res.sendStatus(201)
+    }catch(err){
+        res.sendStatus(500)
+    }
+})
