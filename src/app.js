56e81d69031bb05b337fb1199215731f300d0b15
JeanPiton <jean.orpiton@gmail.com>
2023-07-08 16:57:35 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ import express, { json } from "express"
 import cors from "cors"
 import router from "./routes/index.routes.js"
 
-const PORT = 5000
 const server = express()
 
 server.use(cors())
 server.use(json())
 server.use(router)
 
-server.listen(PORT,()=>{console.log(`Server running at http://localhost:${PORT}`)})
+const port = process.env.PORT || 5000
+server.listen(port,()=>{console.log(`Server running at http://localhost:${port}`)})
