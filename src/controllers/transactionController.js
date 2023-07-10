6bba715842d8e8f5f35414d0ec77f024e42106ae
JeanPiton <jean.orpiton@gmail.com>
2023-07-09 21:50:58 -0300

    Fix delete transaction by id

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -1,5 +1,6 @@
 import { db } from "../database/database.connection.js"
 import dayjs from "dayjs"
+import mongodb from "mongodb"
 
 export async function registerTransaction(req,res){
     const day = dayjs(Date.now()).format("DD/MM")
@@ -25,7 +26,7 @@ export async function listTransactions(req,res){
 
 export async function deleteTransaction(req,res){
     try {
-        const p = await db.collection("transactions").deleteOne({_id:req.params.itemId})
+        const p = await db.collection("transactions").deleteOne({_id:new mongodb.ObjectId(req.params.itemId)})
         res.status(200).send(p)
     } catch (err) {
         res.sendStatus(500)
